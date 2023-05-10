3143df2c0fb28b8e67e682e200d91ae61a54ce0c
Anıl Akpınar <4ni1ak+github@gmail.com>
2023-05-10 15:42:05 +0300

    Examples of arrays and loops lessons

---
+++ BTK-Academy-C-Sharp-Courses/7.1-Arrays/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7._1_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string student1 = "Anıl";
+            string student2 = "Halim";
+            Console.WriteLine(student1);
+            Console.WriteLine(student2);
+            string[] students = new string[3];
+            students[0] = "Anıl";
+            students[1] = "Veli";
+            students[2] = "Halis";
+            foreach (var s in students) {
+                Console.WriteLine(s);
+            };
+            Console.WriteLine();
+            string[,] regions = new string[5, 3]
+            {
+                {"İstanbul","İzmit","Balıkesir" },
+                {"Ankara","Konya","Kırıkkale"},
+                {"Antalya", "Adana", "Mersin"},
+                { "Rize", "Trabzon", "Samsun" },
+                { "İzmir", "Muğla", "Manisa" }
+            };
+            for(int i = 0; i <= regions.GetUpperBound(0); i++)
+            {
+                for(int j=0; j <= regions.GetUpperBound(1); j++)
+                {
+                    Console.WriteLine(regions[i, j]);
+                }
+                Console.WriteLine("********");
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ BTK-Academy-C-Sharp-Courses/8.1-Loops/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8._1_Loops
+{
+    internal class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            Forloop();
+            Whileloop();
+            Dowhileloop();
+            Foreachloop();
+            int number=6;
+            if (IsPrimeNumber(number))
+            {
+                Console.WriteLine("number {0} is a prime number ", number);
+
+            }
+            else
+            {
+                Console.WriteLine("number {0} is not a prime number ", number);
+            }
+        
+        }
+        private static bool IsPrimeNumber(int number)
+        {
+            bool result = true;
+            for (int i=2;i<number-1;i++)
+            {
+                if (number % i == 0)
+                {
+                    result = false;
+                    i = number;
+                }
+            }
+            return result;
+            
+        }
+        static void Foreachloop()
+        {
+            string[] students = new string[3] { "Anıl", "Ahmet", "Mehmet" };
+            foreach(var student in students) {
+                Console.WriteLine(student);
+            }
+
+        }
+        static void Dowhileloop()
+        {
+            int number = 10;
+            do
+            {
+                Console.WriteLine(number);
+                number--;
+            } while (number>=11);
+            Console.WriteLine("Now number is{0}", number);
+        }
+        static void Whileloop() {
+           int number1 = 100;
+            while (number1 >= 0)
+            {
+                Console.WriteLine(number1);
+                number1--;
+            }
+            
+            Console.WriteLine("Now number is{0}",number1);
+        }
+        static void Forloop()
+        {
+            for (int i = 100; i >= 0; i = i - 2)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Finished");
+        }
+
+    }
+}
