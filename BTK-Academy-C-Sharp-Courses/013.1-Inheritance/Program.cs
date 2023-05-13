958a4c3ab0db784db8c630957afbe26ebd4cb918
Anıl Akpınar <4ni1ak+github@gmail.com>
2023-05-14 01:13:03 +0300

    Examples of Inheritance and Inheritance_and_virtual_methods lessons

---
+++ BTK-Academy-C-Sharp-Courses/013.1-Inheritance/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _013._1_Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] persons = new Person[3]
+            {
+                new Coustomer{FirstName="Anıl"},
+                new Student{FirstName= "Kemal"},
+                new Person{FirstName="Ali"}
+            };
+            foreach (Person person in persons)
+            {
+                Console.WriteLine(person.FirstName);
+            }
+            Console.ReadLine();
+        }
+        
+        class Person
+        {
+            public int Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            
+        }
+        class Coustomer : Person
+        {
+            public string City {get; set; }
+        }
+        class Student : Person
+        {
+            public string Department {get; set; }
+        }
+    }
+}
---
+++ BTK-Academy-C-Sharp-Courses/013.2-Inheritance_and_virtual_methods/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _013._2_Inheritance_and_virtual_methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlServer sqlServer = new SqlServer();
+            sqlServer.Add();
+            MySql mySql = new MySql();
+            mySql.Add();
+
+            Console.ReadLine();
+        }
+    }
+    class Database
+    {
+        public  virtual void Add()
+        {
+            Console.WriteLine("Added by Default");
+        }
+        public virtual void Delete()
+        {
+            Console.WriteLine("Deleted by Default");
+        }
+    }
+    class SqlServer : Database
+    {
+        public override void Add()
+        {
+            Console.WriteLine("Added by Sql Code");
+            //base.Add();
+            Console.WriteLine("You can not do this in Interfaces");
+        }
+    }
+    class MySql : Database
+    {
+
+    }
+}
