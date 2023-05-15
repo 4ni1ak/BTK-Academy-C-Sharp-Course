e72fcd35886c4ef302fef8d43e1c043837890afa
Anıl Akpınar <4ni1ak+github@gmail.com>
2023-05-15 22:39:08 +0300

    Examples of 16.1-AbstractClasses lesson

---
+++ BTK-Academy-C-Sharp-Courses/016.1-AccessModifiers/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _016._1_AccessModifiers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    class Customer
+    {
+        protected int Id { get; set; }
+
+        public void Save()
+        {
+            
+        }
+        public void Delete()
+        {
+
+        }
+    }
+    class Student:Customer
+    {
+
+        public void Save2()
+        {
+            
+        }
+    }
+    public class Course
+    {
+        public string Name { get; set; } 
+        private class NestedClass
+        {
+
+        }
+    }
+}
