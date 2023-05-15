b1839dcd3ed1e3b6388dc03c0c0dae2b0b819143
Anıl Akpınar <4ni1ak+github@gmail.com>
2023-05-15 22:40:43 +0300

    Examples of 16.3-AccessModifiers lesson

---
+++ BTK-Academy-C-Sharp-Courses/016.3-AccessModifiers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using 016.-1AccessModifiers //u cant 
+
+//namespace _016._3_AccessModifiers
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Course course = new Course();
+//        }
+//    }
+//}
