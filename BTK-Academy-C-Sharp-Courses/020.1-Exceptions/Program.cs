28023d6f3acf021b12dd179b9a987d6018a9b2b7
Anıl Akpınar <4ni1ak+github@gmail.com>
2023-05-21 23:11:03 +0300

    Example of 020.1-Exceptions lesson

---
+++ BTK-Academy-C-Sharp-Courses/020.1-Exceptions/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _020._1_Exceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ExceptionIntro();
+            try
+            {
+                Find();
+            }
+            catch (RecordNotFoundException exception)
+            {
+                Console.WriteLine(exception.Message);
+                throw;
+            }
+            HandleException(() =>
+            {
+                Find();
+            });
+
+        }
+        private static void HandleException(Action action) 
+        {
+            try
+            {
+                action.Invoke();
+
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+                throw;
+            }
+        }
+
+        private static void Find()
+        {
+            List<string> students = new List<string> { "Anıl", "Eda", "Diyar" };
+            if (!students.Contains("Özlem"))
+            {
+                throw new RecordNotFoundException("Record not found!");
+            }
+            else
+            {
+                Console.WriteLine("Record found");
+            }
+        }
+
+        private static void ExceptionIntro()
+        {
+            try
+            {
+                string[] students = new string[3] { "Anıl", "Eda", "Diyar" };
+                students[3] = "Özlem";
+
+                Console.ReadLine();
+            }
+            catch (IndexOutOfRangeException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+            catch (Exception exception)
+            {
+                //Console.WriteLine(exception.Message);
+                Console.WriteLine(exception.InnerException);
+                throw;
+            }
+
+        }
+    }
+}
