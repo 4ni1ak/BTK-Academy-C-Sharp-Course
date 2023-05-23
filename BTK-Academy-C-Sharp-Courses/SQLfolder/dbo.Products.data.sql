2b825955e2edc79325c796cf510149dea036f900
Anıl Akpınar <4ni1ak+github@gmail.com>
2023-05-23 16:41:00 +0300

    SQL folder added

---
+++ BTK-Academy-C-Sharp-Courses/SQLfolder/dbo.Products.data.sql
@@ -0,0 +1,3 @@
+﻿SET IDENTITY_INSERT [dbo].[Products] ON
+INSERT INTO [dbo].[Products] ([Id], [Name], [UnitPrice], [StockAmount]) VALUES (1, N'Laptop', CAST(3000.0000 AS Money), 21)
+SET IDENTITY_INSERT [dbo].[Products] OFF
