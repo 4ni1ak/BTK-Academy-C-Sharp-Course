f7f2d164c85059a8b53c275df2b5ff9aa4c246f1
Anıl Akpınar <4ni1ak+github@gmail.com>
2023-05-23 16:43:42 +0300

    Example of 021.1-AdoNetDemo part 1 added

---
+++ BTK-Academy-C-Sharp-Courses/021.1-AdoNetDemo/ProductDal.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _021._1_AdoNetDemo
+{
+    internal class ProductDal
+    {
+        public List<Product> GetALL()
+        {
+            SqlConnection connection =  new SqlConnection(@"server=(localdb)\MSSQLLocalDB;initial catalog=ETrade;integrated security=true;");//false;uid=Anil;password=12345"; 
+            if (connection.State == ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+            SqlCommand command = new SqlCommand("Select * from Products",connection);
+            SqlDataReader reader = command.ExecuteReader();                     
+            List<Product> products = new List<Product>();
+
+            while (reader.Read())
+            {
+                Product product = new Product
+                {
+                    Id = Convert.ToInt32(reader["Id"]),
+                    Name = reader["Name"].ToString(),
+                    StockAmount = Convert.ToInt32(reader["StockAmount"]),
+                    UnitPrice = Convert.ToDecimal(reader["UnitPrice"]),
+
+                };
+                products.Add(product);
+                
+            }
+            reader.Close();
+            connection.Close();
+            return products;
+        }
+        public DataTable GetALL2()
+        {
+            SqlConnection connection = new SqlConnection(@"server=(localdb)\MSSQLLocalDB;initial catalog=ETrade;integrated security=true;");//false;uid=Anil;password=12345"; 
+            if (connection.State == ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+            SqlCommand command = new SqlCommand("Select * from Products", connection);
+            SqlDataReader reader = command.ExecuteReader();
+            DataTable dataTable = new DataTable();
+            dataTable.Load(reader);
+            reader.Close();
+            connection.Close();
+
+            return dataTable;
+        }
+    }
+}
--- BTK-Academy-C-Sharp-Courses/SQLFiles/dbo.Products.data.sql
+++
@@ -1,3 +0,0 @@
-﻿SET IDENTITY_INSERT [dbo].[Products] ON
-INSERT INTO [dbo].[Products] ([Id], [Name], [UnitPrice], [StockAmount]) VALUES (1, N'Laptop', CAST(3000.0000 AS Money), 21)
-SET IDENTITY_INSERT [dbo].[Products] OFF
