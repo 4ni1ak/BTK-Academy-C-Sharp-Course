d287cdf7bf202ccaec5e837cdd84f4d2c3b741ee
Anıl Akpınar <4ni1ak+github@gmail.com>
2023-05-29 10:22:57 +0300

    Examples of Generics (1/3)

---
+++ BTK-Academy-C-Sharp-Courses/024.1-Generics/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _024._1_Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    class Product
+    {
+
+    }
+
+    interface IRepository<T>
+    {
+        List<T> GetAll();
+        T Get(int id);
+        void Add(T entity);
+        void Delete(T entity);
+        void Update(T entity);
+    }
+
+    interface IProductDal : IRepository<Product>
+    {
+
+    }
+
+    class ProductDal : IProductDal
+    {
+        public void Add(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Customer
+    {
+
+    }
+
+    interface ICustomerDal : IRepository<Customer>
+    {
+
+    }
+
+    class CustomerDal : ICustomerDal
+    {
+        public void Add(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Customer> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
