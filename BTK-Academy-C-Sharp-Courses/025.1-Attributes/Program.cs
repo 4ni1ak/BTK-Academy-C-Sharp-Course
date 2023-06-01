50516e24d098fd8e791bc78b8771ff1d3cb74c32
Anıl Akpınar <4ni1ak+github@gmail.com>
2023-06-01 11:30:02 +0300

    Examples of Attributes

---
+++ BTK-Academy-C-Sharp-Courses/025.1-Attributes/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _025._1_Attributes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer { Id=1,LastName="Akpinar",Age=32};
+            CustomerDal customerDal = new CustomerDal();
+            customerDal.Add(customer);
+            Console.ReadLine();
+        }
+
+    }
+
+    [ToTable("Customers")]
+    [ToTable("TblCustomers")]
+    class Customer
+    {
+        public int Id { get; set; }
+        
+        [RequiredProperty]
+        public string FirstName { get; set; }
+        [RequiredProperty]
+        public string LastName { get; set; }
+        [RequiredProperty]
+        public int Age { get; set; }
+
+    }
+    class CustomerDal
+    {
+        [Obsolete("Dont use Add, instead use AddNew Method")]
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("{0} {1} {2} {3} Added",
+            customer.Id,customer.FirstName,customer.LastName,customer.Age);
+        }
+        public void AddNew(Customer customer)
+        {
+            Console.WriteLine("{0} {1} {2} {3} Added",
+            customer.Id, customer.FirstName, customer.LastName, customer.Age);
+        }
+    }
+    [AttributeUsage(AttributeTargets.Property,AllowMultiple =true/*| AttributeTargets.Field)*/)]
+    class RequiredPropertyAttribute : Attribute
+    {
+
+    }
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+    class ToTableAttribute: Attribute
+    {
+        string _tableName;
+        public ToTableAttribute(string tableName)
+        {
+            _tableName = tableName;
+        }
+    }
+}
