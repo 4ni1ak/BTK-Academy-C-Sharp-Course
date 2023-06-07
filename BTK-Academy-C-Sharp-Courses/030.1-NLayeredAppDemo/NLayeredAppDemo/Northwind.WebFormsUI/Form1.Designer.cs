3dd2b0bf95a798821f96083aa21d82dab5760b9b
Anıl Akpınar <4ni1ak+github@gmail.com>
2023-06-07 15:04:01 +0300

    Add NLayeredAppDemo project files and SQL queries

---
+++ BTK-Academy-C-Sharp-Courses/030.1-NLayeredAppDemo/NLayeredAppDemo/Northwind.WebFormsUI/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace Northwind.WebFormsUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgwProduct = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwProduct)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgwProduct
+            // 
+            this.dgwProduct.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgwProduct.Location = new System.Drawing.Point(12, 12);
+            this.dgwProduct.Name = "dgwProduct";
+            this.dgwProduct.RowHeadersWidth = 51;
+            this.dgwProduct.RowTemplate.Height = 24;
+            this.dgwProduct.Size = new System.Drawing.Size(1136, 150);
+            this.dgwProduct.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1160, 612);
+            this.Controls.Add(this.dgwProduct);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgwProduct)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgwProduct;
+    }
+}
+
---
+++ SQLfolder/SQLQuery/dbo.Products.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Products] (
+    [Id]          INT          IDENTITY (1, 1) NOT NULL,
+	[Company] VARCHAR(50) NULL,
+    [Name]        VARCHAR (50) NOT NULL,
+    [UnitPrice]   MONEY        NOT NULL,
+    [StockAmount] INT          NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
