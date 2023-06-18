b400e874b300b8948c25e0c0f4fd85670ba9b497
Anıl Akpınar <4ni1ak+github@gmail.com>
2023-06-19 00:14:20 +0300

    036.8-Facade added

---
+++ BTK-Academy-C-Sharp-Courses/036.1-DesignPatterns/DesignPatterns/036.8-Facade/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _036._8_Facade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Save();
+            Console.ReadLine();
+        }
+    }
+
+    public interface ICaching
+    {
+        void Cache();
+    }
+
+    public interface ILogging
+    {
+        void Log();
+    }
+
+    public interface IAuthorize
+    {
+        void CheckUser();
+    }
+
+    class Logging : ILogging
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged");
+        }
+    }
+
+    class Caching : ICaching
+    {
+        public void Cache()
+        {
+            Console.WriteLine("Cached");
+        }
+    }
+
+    class Authorize : IAuthorize
+    {
+        public void CheckUser()
+        {
+            Console.WriteLine("Check User");
+        }
+    }
+
+    class CustomerManager
+    {
+        private CrossCuttingConcernsFacade _concerns;
+        public CustomerManager()
+        {
+            _concerns = new CrossCuttingConcernsFacade();
+        }
+
+        public void Save()
+        {
+            _concerns.Caching.Cache();
+            _concerns.Logging.Log();
+            _concerns.Authorize.CheckUser();
+            Console.WriteLine("Save");
+        }
+    }
+
+    class CrossCuttingConcernsFacade
+    {
+        public ILogging Logging;
+        public ICaching Caching;
+        public IAuthorize Authorize;
+
+        public CrossCuttingConcernsFacade()
+        {
+            Logging = new Logging();
+            Caching = new Caching();
+            Authorize = new Authorize();
+        }
+    }
+
+}
